237258e51f069f43dfaefc7545248d4f3dacc17a
Daniel Kwon <danielkwon89@gmail.com>
2020-05-12 20:51:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,7 +44,9 @@ def monopoly_with_fourth_tier
     {
       pieces: 4,
       names: {
-        reading_railroad: {},
+        reading_railroad: {
+          
+        },
         pennsylvania_railroad: {},
         b_and_o_railroad: {},
         shortline: {}
